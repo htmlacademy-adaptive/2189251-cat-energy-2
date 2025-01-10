b353b095342c9afe78c6c5047b9d84012c0fe47e
Ruzanna.Aslanyan <109111742+Ruza777@users.noreply.github.com>
2025-01-10 18:18:05 +0300

    Module6-task1 (#7)
    * Добавляет крупную сетку страниц
    
    * Выполняет корректировки по крупным сеткам

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,48 @@
-/* в этот файл добавляет скрипты*/
+//скрипт для бургер-меню
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+//скрипт для слайдера главной страницы
+const slider = document.getElementById('slider-handle');
+const imageRight = document.querySelector('.example__image--before');
+
+let isDragging = false;
+
+slider.addEventListener('mousedown', () => {
+  isDragging = true;
+  document.body.style.cursor = 'ew-resize';
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDragging) {
+    return;
+  }
+
+  const rect = slider.parentNode.getBoundingClientRect();
+  let offsetX = e.clientX - rect.left; // Положение курсора относительно контейнера
+  offsetX = Math.max(0, Math.min(offsetX, rect.width)); // Ограничиваем перемещение
+
+  slider.style.left = `${offsetX}px`; // Перемещение линии
+
+  // Изменяем положение изображения
+  const percentage = offsetX / rect.width * 100;
+  imageRight.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`; // Изменяем видимость правого изображения
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+  document.body.style.cursor = 'default';
+});
